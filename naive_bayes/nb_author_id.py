fd2c9f815f29bbba8b9f0ed3dfbbfbcf37e6b347
itaditya <adityaa803@gmail.com>
2017-01-26 15:55:51 +0530

    naive bayes subs

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.4
 
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
@@ -9,19 +9,28 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+# import matplotlib.pyplot as plt
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
+gnb = GaussianNB()
+t0 = time()
+labels_fit = gnb.fit(features_train, labels_train)
+print("training time:", round(time() - t0, 3), "s")
+t1 = time()
+labels_pred = labels_fit.predict(features_test)
+print("predict time:", round(time() - t1, 3), "s")
+print("Accuracy is:", accuracy_score(labels_test, labels_pred))
 
 
 #########################################################
@@ -29,5 +38,3 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-
-
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,40 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
+features_train = features_train[:len(features_train) // 100]
+labels_train = labels_train[:len(labels_train) // 100]
 
 #########################################################
 ### your code goes here ###
+# c_list = [10.0, 100, 1000, 10000]
+# c_list = [10000]
+# for c_item in c_list:
+#     print("C is", c_item)
+#     clf = SVC(kernel="rbf", C=c_item)
+#     t0 = time()
+#     labels_fit = clf.fit(features_train, labels_train)
+#     print("training time:", round(time() - t0, 3), "s")
+#     t1 = time()
+#     labels_pred = labels_fit.predict(features_test)
+#     print("predict time:", round(time() - t1, 3), "s")
+#     print("Accuracy is:", accuracy_score(labels_test, labels_pred))
+#     print("-----------------")
 
-#########################################################
-
-
+clf = SVC(kernel="rbf", C=10000)
+labels_fit = clf.fit(features_train, labels_train)
+labels_pred = labels_fit.predict(features_test[10])
+print(labels_pred)
+####################################
